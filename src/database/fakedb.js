a0b6202ed6569bdb96c6972daca6129c9b993167
Lucas Mansur <38110444+lucasmansur@users.noreply.github.com>
2020-08-09 11:53:31 -0300

    Add files via upload
    Completo a semana NLW, com primeiro desafio concluindo.

---
+++ src/database/fakedb.js
@@ -0,0 +1,38 @@
+//dados da aplicacao
+const proffys = [
+    {
+    name: "Diego Fernades",
+    avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
+    whatsapp: "21 9998889",
+    bio: "Entusiasta das melhores tecnologias de química avançada.Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
+    subject: "Quimica",
+    cost: "20",
+    weekday: [0],
+    time_from: [720],
+    time_to: [1220] 
+    },
+    {
+        name: "Daniele Evangelista",
+        avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
+        whatsapp: "21 81798889",
+        bio: "Entusiasta das melhores tecnologias de química avançada.Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
+        subject: "Fisica",
+        cost: "15",
+        weekday: [1],
+        time_from: [720],
+        time_to: [1220] 
+    }, 
+    {
+            name: "Mayk Brito",
+            avatar: "https://avatars2.githubusercontent.com/u/6643122?s=460&u=1e9e1f04b76fb5374e6a041f5e41dce83f3b5d92&v=4",
+            whatsapp: "21 81798889",
+            bio: "Entusiasta das melhores tecnologias de química avançada.Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
+            subject: "Geografia",
+            cost: "5",
+            weekday: [4],
+            time_from: [720],
+            time_to: [1220] 
+    }
+        
+]
+
